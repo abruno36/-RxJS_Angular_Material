cc0f5ce7b87b62ced6621ec58965fba02535249b
antonio.bruno <antonio.bruno@animaeducacao.com.br>
2024-07-14 09:21:58 -0300

    Atualização Users - 14070921

--- ProjectSchool/src/app/pages/users/users.module.ts
+++ ProjectSchool/src/app/pages/users/users.module.ts
@@ -3,15 +3,23 @@ import { CommonModule } from '@angular/common';
 
 import { UsersRoutingModule } from './users-routing.module';
 import { UsersComponent } from './users.component';
-
+import { MyProfilesComponent } from './my-profiles/my-profiles.component';
+import { MyConquestsComponent } from './my-conquests/my-conquests.component';
+import { MyDisciplinesComponent } from './my-disciplines/my-disciplines.component';
+import { UsersMaterialModule } from 'src/app/shared/materials/users-mat.module';
 
 @NgModule({
   declarations: [
-    UsersComponent
+    UsersComponent,
+    MyProfilesComponent,
+    MyConquestsComponent,
+    MyDisciplinesComponent
   ],
   imports: [
     CommonModule,
+    UsersMaterialModule,
     UsersRoutingModule
+    
   ]
 })
 export class UsersModule { }
